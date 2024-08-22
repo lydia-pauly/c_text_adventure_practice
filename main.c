d4eaf0ed8063b80bef873e06efc4398d2a45a14a
Lydia Pauly <pauly.lydia@gmail.com>
2024-08-22 22:28:40 +0100

    Added in pseudorandom generator using time(0) as seed to keep generating randomness, scoped to 10

--- main.c
+++ main.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
-main() 
+#define	RAND_MAX_MATRIX	10 /* Sets max limit for matrix */
+
+
+int main() 
 {
 	printf("Welcome to [TEXT ADVENTURE]\n");
+	srand(time(0));
+	printf("%d\n", rand() % RAND_MAX_MATRIX);
+	return 0;
 }
